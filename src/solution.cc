86a84cb5e8318191017930b8c51afcb493850940
EDogCPS <ejmcphilliam@cps.edu>
2025-02-17 04:42:01 +0000

    modified:   src/solution.cc

--- src/solution.cc
+++ src/solution.cc
@@ -7,19 +7,19 @@
 Student ReadStudentRecFromStream(std::istream& is) {
   char comma;
   Student student {}; 
-    std::string first_name;
-    std::string last_name;
+  std::string first_name;
+  std::string last_name;
 
-    is >> first_name;
-    is >> last_name ;
-    is >> student.uin;
-    is >> comma >> student.gpa;
+  is >> first_name;
+  is >> last_name ;
+  is >> student.uin;
+  is >> comma >> student.gpa;
 
-    last_name.pop_back();
-    student.full_name = first_name + " " + last_name;
-    if (is.fail()) {
-      return Student{};
-    }
+  last_name.pop_back();
+  student.full_name = first_name + " " + last_name;
+  if (is.fail()) {
+    return Student{};
+  }
 
   return student;
 }
